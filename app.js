46ce094b4b600e8438e9276facb24e6641b52112
lindelani4435 <95017550+lindelani4435@users.noreply.github.com>
2022-03-11 08:38:04 +0200

    Lindelani

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const cors = require("cors");
 
+
 const app = express();
 
 app.use(cors());
 app.use(bodyparser.json());
+
+//conne
 
 app.listen(5000,()=>
 {
--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password:'',
-    database: 'user',
+    database: 'tvh_dbh',
     port:3306
 })
 
@@ -30,37 +30,49 @@ db.connect(err =>{
 })
 
 
-//get admin data
+app.post('/registration', (req, res) =>{
+    console.log(req.body);
 
+  
+  let first_last_name = req.body.first_last_name;
+  let gender = req.body.gender;
+  let birth_date = req.body.birth_date;
+  let student_mail = req.body.student_mail;
+  let institution = req.body.institution;
+  let registration_proof = req.body.registration_proof;
+  let mobile_number = req.body.mobile_number;
+  let programing_skills = req.body.programing_skills;
+  let applicant_status =req.body.applicant_status;
+  let information_source = req.body.information_source;
+  let participant_id = req.body.participant_id;
 
+    let qr = `INSERT INTO application ( first_last_name, gender, birth_date, student_mail, institution, registration_proof, mobile_number, programing_skills, applicant_status, information_source, participant_id)
+     VALUES ("${first_last_name}","${gender}","${birth_date}","${student_mail}","${institution}","${registration_proof}","${mobile_number}","${programing_skills}","${applicant_status}","${information_source}","${participant_id}")`;
+    db.query(qr,(err,result)=>
+    {
+        if(err){console.log(err)}
 
-
-app.get('/admin', (req, res) =>{
-    
-
-    let qr = 'select email, password, admin_name FROM admin WHERE email = "sanele@gmail.com" AND password ="sanele" AND admin_name = "Mzwe" ';
-    db.query(qr, (err, result) => {
-
-        if(err)
-        {
-            console.log(err,'errs');
+        if (result.length>0)
+        {   
+            res.send(
+                {
+                    message:"Data Captured..."
+                });
         }
-        if(result.length >0)
-        {
-            res.send({
-
-                message: 'loged in',
-                data:result
-            });
+        else{
+            res.send(
+                {
+                    message:"Enter all fields..."
+                })
         }
-    });
+    } );
 });
 
 
 
 
 
-app.listen(9002,() =>{
+app.listen(9500,() =>{
 
-    console.log('server running....');
+    console.log('server running....On port');
 }); 
