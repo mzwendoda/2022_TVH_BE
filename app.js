ee2c3cbf731b54178e0f85c4e71271f3d8fe1b1c
lindelani4435 <95017550+lindelani4435@users.noreply.github.com>
2022-04-01 04:02:43 +0200

    Done with creation and validation of signUp and signUp APIs

--- app.js
+++ app.js
@@ -1,32 +1,18 @@
-const express = require("express");
-const bodyparser = require("body-parser");
-const dbconnection = require("./connection/db");
-const user = require("./router/user");
+const express = require('express');
+const bodypaser = require('body-parser');
+const signUp = require('./routers/signUp');
+const signIn = require("./routers/signIn");
 const app = express();
 
-//dbconnection varification
-dbconnection.authenticate()
-.then(()=>
-{
-    console.log("Database connection sucessfully estamblished!!!");
-}).catch((err)=>
-{
-    console.log("Database connection NOT estamblished!!!",err);
-})
+app.use(bodypaser.json());
+app.unsubscribe(bodypaser.urlencoded({extended:false}));
 
-app.use(bodyparser.json());
-app.unsubscribe(bodyparser.urlencoded({
-        extended:true
-    }))
-
-//Calling The Routers
-app.use("/",user);
-
-app.get('/', function (req, res) {
-    res.send("App Running👌👌👌");
+app.use("/",signUp);
+app.use("/",signIn);
+app.use('/', function(res, res){
+    res.send("App Running👌👌👌(PostMan check routers)")
 });
 
-//lisignig port
-app.listen({port: 5005}, async() =>{
-    // await sequelize.authenticate();
+app.listen(9500,()=>{
+    console.log("Express server Up and Running in port 9500....");
 });
---
+++ connection/dbh.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+
+const dataBase = mysql.createConnection({
+
+    host: 'localhost',
+    user: 'root',
+    password:'',
+    database: 'tvh_dbh',
+    port:3306
+})
+
+//connection check
+
+dataBase.connect(err =>{
+
+    if(err) {console.log('err');}
+    console.log('database connected..');
+});
+
+module.exports = dataBase;
