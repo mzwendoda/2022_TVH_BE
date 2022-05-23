f060e77e8e85f13d93e1671c29fb1d6a970c96ab
mzwendoda <95077324+mzwendoda@users.noreply.github.com>
2022-05-23 10:06:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use('/public/uploads',express.static('public/uploads'))
 
 const db = mysql.createConnection({
 
-    host: 'db-tvh2022-inst.cvqpj5ith2h1.us-east-1.rds.amazonaws.com',
+    host: 'tvh-db-inst.c1wcxiext4vo.us-east-1.rds.amazonaws.com',
     user: 'admin',
     password:'tvh2022icep',
     database: 'tvhdb',
