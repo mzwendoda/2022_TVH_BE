7ddf509d2c459bcfab596d5af7475719b2f08213
mzwendoda <95077324+mzwendoda@users.noreply.github.com>
2022-05-27 01:49:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,18 @@ app.use(fileupload());
 app.use(cors());
 app.use(bodypaser.json());
 
+app.use(cors({origin: '*'}));
+
+
+app.use(function(req, res, next) {
+    //Header allowences of METHODS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With','Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.use(express.static('public'))
 app.use('/public/uploads',express.static('public/uploads'))
 
